52b263cf3805bf778fadb7234008a8e356228528
Martind64 <martind_64@hotmail.com>
2017-03-19 21:25:17 +0100

    changed register page a bit

--- views/register.php
+++ views/register.php
@@ -7,27 +7,32 @@
 <body>
 
 <div class="container">
-<div class="col-lg-4">
+<div class="col-lg-5">
 Register page!
 <a href="index.php">go back</a>
 	
 <form action="../controllers/user/create.php" method="POST">
 <div class="form-group">
-	<label>firstname</label>
-	<input class="form-control" type="text" name="firstname">
-	<label>lastname</label>
-	<input class="form-control" type="text" name="lastname">
-	<label>username</label>
-	<input class="form-control" type="text" name="username">
-	<label>email</label>
-	<input class="form-control" type="text" name="email">
-	<label>address</label>
-	<input class="form-control" type="text" name="address">
-	<label>phone Number</label>
-	<input class="form-control" type="text" name="phoneNo">
-	<label>password</label>
-	<input class="form-control" type="text" name="password">
-	<button type="submit" class="btn btn-primary">Register</button>
+	<div class="col-lg-6">
+		<label>firstname</label>
+		<input class="form-control" type="text" name="firstname">
+		<label>username</label>
+		<input class="form-control" type="text" name="username">
+		<label>address</label>
+		<input class="form-control" type="text" name="address">
+		<label>password</label>
+		<input class="form-control" type="text" name="password">
+	</div>
+	<div class="col-lg-6">
+		<label>lastname</label>
+		<input class="form-control" type="text" name="lastname">
+		<label>email</label>
+		<input class="form-control" type="text" name="email">
+		<label>phone Number</label>
+		<input class="form-control" type="text" name="phoneNo">
+		<button style="margin-top: 12%;" type="submit" class="btn btn-primary">Register</button>
+	</div>
+
 	</div>
 </div>
 
